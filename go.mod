f5b5abbeefa80d7ab937708c51a1c2f3d2a1845d
Rico <d-Rickyy-b@users.noreply.github.com>
2021-07-03 03:02:26 +0200

    feat: initial version of myShrugBot

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/d-Rickyy-b/myShrugBot
+
+go 1.16
+
+require gopkg.in/tucnak/telebot.v2 v2.3.5
---
+++ main.go
@@ -0,0 +1,147 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"github.com/d-Rickyy-b/myShrugBot/config"
+	tb "gopkg.in/tucnak/telebot.v2"
+	"log"
+	"strconv"
+	"time"
+	"unicode/utf8"
+)
+
+// define bot in global context to be accessible by all handlers
+var bot *tb.Bot
+
+// ellipsis takes a text and an integer to generate a new string that's <= 'max' characters (not bytes) long.
+// Excess characters will be removed from the middle and replaced with "..."
+func ellipsis(text string, max int) string {
+	// subtract length of "..."
+	max -= 5
+
+	if max <= 5 {
+		max = 5
+	}
+
+	if len(text) <= max {
+		return text
+	}
+
+	charCount := 0
+
+	endFirst := max / 2
+	startLast := utf8.RuneCountInString(text) - (max / 2)
+	result := ""
+
+	for _, r := range text {
+		charCount++
+
+		if charCount > endFirst && charCount <= startLast {
+			continue
+		} else if charCount == endFirst {
+			result += string(r)
+			result += " ... "
+			continue
+		}
+
+		result += string(r)
+	}
+
+	return result
+}
+
+// shrugHandler handles all the Telegram inline queries.
+// It responds to incoming inline queries with a set of different options.
+// 1) The classic shrug
+// 2) The (male) shrug emoji
+// 3) The text the user entered + classic shrug
+// 4) The text the user entered + shrug emoji
+func shrugHandler(q *tb.Query) {
+	shrugs := []string{
+		"¯\\_(ツ)_/¯",
+		"🤷‍♂️",
+	}
+
+	if q.Text != "" {
+		shrugs = append(shrugs, q.Text+" ¯\\_(ツ)_/¯")
+		shrugs = append(shrugs, q.Text+" 🤷‍♂️")
+	}
+
+	results := make(tb.Results, len(shrugs)) // []tb.Result
+	for i, shrug := range shrugs {
+		result := &tb.ArticleResult{
+			ResultBase: tb.ResultBase{},
+			Title:      ellipsis(shrug, 45),
+			Text:       shrug,
+			HideURL:    true,
+		}
+
+		results[i] = result
+		// needed to set a unique string ID for each result
+		results[i].SetResultID(strconv.Itoa(i))
+	}
+
+	err := bot.Answer(q, &tb.QueryResponse{
+		Results:   results,
+		CacheTime: 60, // a minute
+	})
+
+	if err != nil {
+		log.Println(err)
+	}
+}
+
+// setupBot makes all the initializations and configurations for running the Telegram bot
+//
+func setupBot(c config.Config) {
+	var poller tb.Poller
+
+	if c.Webhook.Enabled {
+		// Currently we don't support all the fields of Webhook.
+		// We could add stuff like TLS/Cert config later still
+		poller = &tb.Webhook{
+			Listen: c.Webhook.Listen,
+			Endpoint: &tb.WebhookEndpoint{
+				PublicURL: c.Webhook.Url,
+			},
+		}
+		log.Println("Using webhook from config!")
+	} else {
+		poller = &tb.LongPoller{Timeout: 10 * time.Second}
+		log.Println("Using long polling as fallback!")
+	}
+
+	b, err := tb.NewBot(tb.Settings{
+		Token:  c.Token,
+		Poller: poller,
+	})
+
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+	bot = b
+
+	b.Handle("/start", func(m *tb.Message) {
+		welcomeText := fmt.Sprintf("Hi! Thanks for using this bot. You can just type '@%s' in any chat and I'll provide you with some shruggies ¯\\_(ツ)_/¯", b.Me.Username)
+		b.Send(m.Sender, welcomeText)
+	})
+
+	b.Handle(tb.OnQuery, shrugHandler)
+
+	// We must remove the webhook if we want to use Polling. Otherwise we'll run into an error and can't poll new updates.
+	if !c.Webhook.Enabled {
+		b.RemoveWebhook()
+	}
+	log.Printf("Starting @%s", b.Me.Username)
+	b.Start()
+}
+
+func main() {
+	var configFile = flag.String("config", "config.json", "Path to config file")
+	flag.Parse()
+
+	c, _ := config.ReadConfig(*configFile)
+	setupBot(c)
+}
